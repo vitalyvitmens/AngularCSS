db9d6c0872ddc3092b89fd298463c4d3f3be7c9c
vitalyvitmens <otm_@tut.by>
2023-02-20 16:19:17 +0300

    add data in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { ProductsService } from './services/products.service';
+// import { products as data } from './data/products'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'AngularCSS';
+export class AppComponent implements OnInit {
+  title = 'ООО «ЦЕНТРОСТАЛЬСТРОЙ»';
+  products: IProduct[] = [];
+  loading = false;
+
+  constructor(private productsService: ProductsService) {}
+
+  // products: IProduct[] = data
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.productsService.getAll().subscribe((products) => {
+      this.products = products;
+      this.loading = false;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
